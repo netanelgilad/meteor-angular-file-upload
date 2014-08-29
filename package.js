abc292058464560998e5dd3474ac20fb76a79f4c
Netanel Gilad <netanelgilad@gmail.com>
2014-08-29 22:37:21 +0300

    changes for 0.9.0

--- package.js
+++ package.js
@@ -1,26 +1,13 @@
 Package.describe({
-  summary: "Angular File Upload is a module for the AngularJS framework"
+  summary: "Angular File Upload is a module for the AngularJS framework",
+  version: "0.0.3",
+  git: "https://github.com/netanelgilad/meteor-angular-file-upload.git"
 });
 
-function packageExists(pkgname) {
-  var fs = Npm.require('fs');
-  var path = Npm.require('path');
-  var pkgpath = path.join('packages', pkgname);
-  return fs.existsSync(pkgpath);
-}
-
 Package.on_use(function (api) {
-  api.use('bower', 'client');
-
-  if (packageExists('angularite')) {
-    api.use('angularite', 'client');
-  } else if (packageExists('ngMeteor')) {
-    api.use('ngMeteor', 'client');
-  }
-
-  // Install bower components.
-  api.add_files('smart.json', 'client');
+  api.addFiles('angular-file-upload.min.js', 'client');
+  api.addFiles('angular-file-upload.min.map', 'client');
 
   // Client files.
-  api.add_files('init.js', 'client');
+  api.addFiles('init.js', 'client');
 });
