8e91f6749092b9ad383745d706a00b5e645f51c6
Netanel Gilad <netanelgilad@gmail.com>
2015-01-07 21:50:44 +0200

    update angular version

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   summary: "Angular File Upload is a module for the AngularJS framework",
-  version: "0.0.3",
+  version: "0.0.4",
   git: "https://github.com/netanelgilad/meteor-angular-file-upload.git"
 });
 
 Package.on_use(function (api) {
   api.use('mquandalle:bower@0.1.11', 'client');
-  api.use('urigo:angular@0.5.7', 'client');
+  api.use('urigo:angular@0.6.0-alpha', 'client');
 
   // Install bower components.
   api.addFiles('smart.json', 'client');
