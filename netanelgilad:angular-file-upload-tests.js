7f975710479ed63af0df271b24ae63d5a385037a
David Yahalomi <davidyaha@gmail.com>
2015-03-10 22:03:19 +0200

    Upgrading angular-meteor to 0.7.2 and removing bower

--- netanelgilad:angular-file-upload-tests.js
+++ netanelgilad:angular-file-upload-tests.js
@@ -1,5 +1,6 @@
-// Write your tests here!
-// Here is an example.
-Tinytest.add('example', function (test) {
-  test.equal(true, true);
+
+Tinytest.add('Load dependencies', function (test) {
+  test.isNotNull(angular, 'Angular exists as global variable');
+  test.isNotNull(angular.module('angular-meteor'), 'angular-meteor exists');
+  test.isNotNull(angular.module('angularFileUpload', 'angularFileUpload exists'));
 });
--- package.js
+++ package.js
@@ -1,16 +1,25 @@
 Package.describe({
   summary: "Angular File Upload is a module for the AngularJS framework",
-  version: "0.0.4",
+  version: "0.0.5",
+  name: 'netanelgilad:angular-file-upload',
   git: "https://github.com/netanelgilad/meteor-angular-file-upload.git"
 });
 
-Package.on_use(function (api) {
-  api.use('mquandalle:bower@0.1.11', 'client');
-  api.use('urigo:angular@0.6.0-alpha', 'client');
+Package.onUse(function (api) {
+  api.versionsFrom('0.9.0');
+  api.use('urigo:angular@0.7.2', 'client');
 
-  // Install bower components.
-  api.addFiles('smart.json', 'client');
+  // Load angular-file-upload lib.
+  api.addFiles('lib/bower_components/angular-file-upload/angular-file-upload.min.js', 'client');
 
-  // Client files.
+  // Support for old versions of angular-meteor
   api.addFiles('init.js', 'client');
+});
+
+Package.onTest(function (api) {
+  api.use('tinytest');
+
+  api.use('netanelgilad:angular-file-upload');
+
+  api.addFiles('netanelgilad:angular-file-upload-tests.js', 'client');
 });
